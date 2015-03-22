d21c38504f904ae3a71d96e41aeb33a10102c51b
Leo <1093038078@qq.com>
2015-03-22 20:15:49 +0800

    add ios ui project 01

---
+++ LearnIOSUI/L01StoryBoard/L01StoryBoard/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  L01StoryBoard
+//
+//  Created by Leo Liu on 15/3/17.
+//  Copyright (c) 2015年 eoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Hello world ");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
