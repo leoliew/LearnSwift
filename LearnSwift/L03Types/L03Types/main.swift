b2b7e9ebbbfd82d7c54625d249bbc23df1d3db55
Leo <1093038078@qq.com>
2015-04-02 00:40:30 +0800

    add string lesson

--- LearnSwift/L03Types/L03Types/main.swift
+++ LearnSwift/L03Types/L03Types/main.swift
@@ -50,7 +50,7 @@ keyValue.description
 if !keyValue {}
 //3.3在控制流中布尔型作为条件
 var SurplusDate = 3
-while SurplusDate>=0 {}
+//while SurplusDate>=0 {}
 
 
 //4.字符串
@@ -60,6 +60,67 @@ var sampleStr:String
 var keyNote="someNote"
 //4.2字符串连接,通过 “＋” 连接起来
 sampleStr = "keyNote"+"is not null"
+//4.3判断字符串是否有值
+if sampleStr.isEmpty{
+    println("sampleStr is null")
+}
+//4.4比较字符串是否相等，使用 “==”
+let str1 = "North always been open"
+let str2 = "North always been open"
+if str1==str2{
+    println("These two string are full equal")
+}
+
+
+//5.字符
+//5.1声明
+var myChar:Character
+    //声明的同时初始化
+var doller:Character = "$"
+//5.2统计字符个数需要使用 countElements 函数获取字符串中的字符数量
+let LogTitle = "Swift is a new language"
+println("LogTitle has \(countElements(LogTitle)) characters")
+
+
+//6.统计log demo
+var ExceptionLogs=[
+    "Warning: Login In System with no password Check By DB",
+    "Warning: View customer list without DB",
+    "Error: You have no Jurisdiction",
+    "Warning: A Operate is no effect",
+    "Error: Illicit close program"]
+
+var warningCount = 0
+var errorCount = 0
+//现在我们可以用hasPrefix 方法来统计日志中警告和错误提醒的数量
+for atitle in ExceptionLogs{          //使用for in 遍历字符串日志数组
+    if atitle.hasPrefix("Warning") {   //判断字符串头是否包含Warning
+        ++warningCount                    //如果包含则count+1
+    }
+    if atitle.hasPrefix("Error") {     //判断字符串头是否包含Error
+        ++errorCount
+        //关于前置和后置加加运算我们会在后面详细介绍
+    }
+}
+println("warning have \(warningCount) and error have \(errorCount).")
+//输出warning have 3 and error have 2.
+
+var DBCount = 0
+//然后使用hasSuffix 方法来统计哪些操作和数据库进行了交互
+for atitle in ExceptionLogs{
+    //使用for in 遍历字符串日志数组
+    if atitle.hasSuffix ("Check By DB") {
+        //判断字符串尾是否包含Check By DB
+        ++DBCount //如果包含则count+1
+    }
+}
+println("Have \(DBCount) Count Check By DB")
+//输出：Have 1 Count Check By DB
+
+
+
+
+
 
 
 
